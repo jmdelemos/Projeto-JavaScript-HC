25e578d720a92c2512f771f6246560096c983306
jmdelemos <jmdelemos@hotmail.com>
2021-07-14 01:58:19 -0300

    Arquivo app.js finalizado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const livros = require('./database')
 const readline = require('readline-sync')
 
 // question permite fazer perguntas ao usuário
-const entradaInicial = readline.question('Deseja buscar um livro? S/N')
+const entradaInicial = readline.question('Deseja buscar um livro? S/N\n')
 
 // Verificação do valor obtido pela variavel entradaInicial
 
@@ -19,13 +19,22 @@ if (entradaInicial.toLocaleUpperCase() === 'S'){
     console.log('Estas são as categorias disponíveis: ')
     console.log('Produtividade', '/História brasileira', '/Américas', '/Estilo de vida', '/Tecnologia')
     
-    const entradaCategoria = readline.question('Qual categoria você escolhe?')
+    const entradaCategoria = readline.question('Qual categoria você escolhe?\n')
     
     // Busca os livros da categoria passada pelo usuário
     const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
 
     console.table(retorno)
+}else{
+    // Caso resposta seja Não ("N"), mostra todos os livros 
+    // ordenados pelo número de páginas
+
+    const livrosOrdenados = livros.sort((a,b) => a.paginas - b.paginas) // Compara o número de paginas a cada 2 libros
+    
+    console.log('Estes são todos os livros disponíveis')
+    console.table(livrosOrdenados)
+
+
+
 }
 
-// Caso resposta seja Não ("N"), mostra todos os livros 
-// ordenados pelo número de páginas
