5299e62f4ec8af5d9754db2b7287922d8116edf2
jmdelemos <jmdelemos@hotmail.com>
2021-07-14 01:46:38 -0300

    Ajuste da categoria produtividade no arquivo database.js. Implementação da resposta SIM do usuário no arquivo app.js

--- app.js
+++ app.js
@@ -8,4 +8,24 @@ const livros = require('./database')
 const readline = require('readline-sync')
 
 // question permite fazer perguntas ao usuário
-const entrada_inicial = readline.question('Deseja buscar um livro? S/N')
+const entradaInicial = readline.question('Deseja buscar um livro? S/N')
+
+// Verificação do valor obtido pela variavel entradaInicial
+
+/* Caso a resposta seja Sim ("S"), mostra as categorias disponíveis,
+ em seguida pergunta qual a categoria a ser escolhida.*/
+
+if (entradaInicial.toLocaleUpperCase() === 'S'){
+    console.log('Estas são as categorias disponíveis: ')
+    console.log('Produtividade', '/História brasileira', '/Américas', '/Estilo de vida', '/Tecnologia')
+    
+    const entradaCategoria = readline.question('Qual categoria você escolhe?')
+    
+    // Busca os livros da categoria passada pelo usuário
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
+
+    console.table(retorno)
+}
+
+// Caso resposta seja Não ("N"), mostra todos os livros 
+// ordenados pelo número de páginas
